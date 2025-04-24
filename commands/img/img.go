29bdf1b0ae02d7dbfdda2b7b7de76af9d4bd866e
Fluffy-Bean <michal-gdula@protonmail.com>
2025-04-24 21:18:44 +0100

    Experimental image captions

--- commands/img/img.go
+++ commands/img/img.go
@@ -1,10 +1,20 @@
 package img
 
 import (
+	"bufio"
+	"bytes"
+	_ "embed"
 	"errors"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"image/png"
+	"io"
 	"net/http"
+	"strings"
 	"time"
 
+	"git.sr.ht/~sbinet/gg"
 	"github.com/Fluffy-Bean/lynxie/app"
 	"github.com/Fluffy-Bean/lynxie/utils"
 	"github.com/bwmarrin/discordgo"
@@ -16,10 +26,15 @@ var client = http.Client{
 	Timeout: 10 * time.Second,
 }
 
+//go:embed resources/Impact.ttf
+var resourceImpactFont []byte
+
 func RegisterImgCommands(a *app.App) {
 	a.RegisterCommand("saveable", registerSaveable(a))
-
 	a.RegisterCommandAlias("gif", "saveable")
+
+	a.RegisterCommand("caption", registerCaption(a))
+	a.RegisterCommandAlias("c", "caption")
 }
 
 func registerSaveable(a *app.App) app.Callback {
@@ -67,9 +82,144 @@ func registerSaveable(a *app.App) app.Callback {
 			},
 			Files: []*discordgo.File{
 				{
-					Name:        "image.gif",
+					Name:        "saveable.gif",
 					ContentType: "image/gif",
 					Reader:      res.Body,
+				},
+			},
+			Reference: h.Reference,
+		})
+
+		return app.Error{}
+	}
+}
+
+func registerCaption(a *app.App) app.Callback {
+	return func(h *app.Handler, args []string) app.Error {
+		fileEndpoint, err := getClosestImage(h)
+		if err != nil {
+			return app.Error{
+				Msg: "Could not get image",
+				Err: err,
+			}
+		}
+
+		req, err := http.NewRequest(http.MethodGet, fileEndpoint, nil)
+		if err != nil {
+			return app.Error{
+				Msg: "",
+				Err: err,
+			}
+		}
+
+		if req.ContentLength > maxFileSize {
+			return app.Error{
+				Msg: "Could not get image",
+				Err: errors.New("requested file is too big"),
+			}
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			return app.Error{
+				Msg: "",
+				Err: err,
+			}
+		}
+		defer res.Body.Close()
+
+		buff, err := io.ReadAll(res.Body)
+		if err != nil {
+			return app.Error{
+				Msg: "Failed to read image",
+				Err: err,
+			}
+		}
+
+		var img image.Image
+		switch http.DetectContentType(buff) {
+		case "image/png":
+			img, err = png.Decode(bytes.NewReader(buff))
+			if err != nil {
+				return app.Error{
+					Msg: "Failed to decode PNG",
+					Err: err,
+				}
+			}
+			break
+		case "image/jpeg":
+			img, err = jpeg.Decode(bytes.NewReader(buff))
+			if err != nil {
+				return app.Error{
+					Msg: "Failed to decode JPEG",
+					Err: err,
+				}
+			}
+			break
+		default:
+			return app.Error{
+				Msg: "Unknown or unsupported image format",
+				Err: errors.New("Unknown or unsupported image format " + http.DetectContentType(buff)),
+			}
+		}
+
+		fontSize := float64(img.Bounds().Dx() / 25)
+		if fontSize < 16 {
+			fontSize = 16
+		} else if fontSize > 50 {
+			fontSize = 50
+		}
+
+		canvas := gg.NewContext(img.Bounds().Dx(), img.Bounds().Dy()+200)
+		err = canvas.LoadFontFaceFromBytes(resourceImpactFont, fontSize)
+		if err != nil {
+			return app.Error{
+				Msg: "Failed to load font",
+				Err: err,
+			}
+		}
+
+		canvas.SetColor(color.RGBA{R: 255, G: 255, B: 255, A: 255})
+		canvas.Clear()
+
+		canvas.SetColor(color.RGBA{R: 0, G: 0, B: 0, A: 255})
+		canvas.DrawStringWrapped(
+			strings.Join(args, " "),
+			float64(img.Bounds().Dx()/2),
+			100,
+			0.5,
+			0.5,
+			float64(img.Bounds().Dx()),
+			1.5,
+			gg.AlignCenter,
+		)
+
+		canvas.DrawImage(img, 0, 200)
+
+		var export bytes.Buffer
+		err = canvas.EncodeJPG(bufio.NewWriter(&export), &jpeg.Options{
+			Quality: 100,
+		})
+		if err != nil {
+			return app.Error{
+				Msg: "Failed to encode JPEG",
+				Err: err,
+			}
+		}
+
+		h.Session.ChannelMessageSendComplex(h.Message.ChannelID, &discordgo.MessageSend{
+			Embed: &discordgo.MessageEmbed{
+				Title: "Caption",
+				Image: &discordgo.MessageEmbedImage{
+					URL: "attachment://caption.jpeg",
+				},
+				Color: utils.ColorFromRGB(255, 255, 255),
+			},
+			Files: []*discordgo.File{
+				{
+					Name:        "caption.jpeg",
+					ContentType: "image/jpeg",
+					Reader:      bytes.NewReader(export.Bytes()),
 				},
 			},
 			Reference: h.Reference,
--- go.mod
+++ go.mod
@@ -2,10 +2,18 @@ module github.com/Fluffy-Bean/lynxie
 
 go 1.24.0
 
-require github.com/bwmarrin/discordgo v0.28.1
+require (
+	git.sr.ht/~sbinet/gg v0.6.0
+	github.com/bwmarrin/discordgo v0.28.1
+)
 
 require (
+	github.com/campoy/embedmd v1.0.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/image v0.21.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 )
